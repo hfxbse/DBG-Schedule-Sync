49154e00cc8784712d4b721a9e066d1db815cf75
stubit <haas.fabian@gmx.de>
2020-11-10 21:17:27 +0100

    add logout button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
-  <setup v-if="online">
-  </setup>
+  <div v-if="online">
+    <button @click="logout">Ausloggen</button>
+    <setup/>
+  </div>
   <center-container v-else>
     <h1>You're offline.</h1>
   </center-container>
@@ -9,6 +11,12 @@
 <script>
 import Setup from '@/components/Setup.vue'
 import Center from '@/components/Center';
+import * as firebase from "firebase";
+
+const auth = async () => {
+  await import(/* webpackChunkName: "firebase_auth"*/ 'firebase/auth')
+  return firebase.auth()
+}
 
 export default {
   name: 'Home',
@@ -24,6 +32,40 @@ export default {
     return {
       online: window.navigator.onLine
     }
+  },
+  methods: {
+    async logout() {
+      await (await auth()).signOut()
+      await this.$router.push({name: 'Auth'})
+    }
   }
 }
 </script>
+
+<style scoped>
+div {
+  position: relative;
+}
+
+button {
+  display: block;
+
+  position: fixed;
+  top: 1rem;
+  left: 1rem;
+
+  z-index: 1;
+
+  font-size: 1rem;
+  font-weight: 200;
+
+  background: rgba(255, 255, 255, 0.7);
+  padding: .5rem 1rem;
+
+  border: none;
+  border-radius: 5rem;
+  box-shadow: 0 3px 3px rgba(0, 0, 0, 0.2);
+
+  outline: none;
+}
+</style>
