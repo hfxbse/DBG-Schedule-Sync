3c3981fdae5830f7a92be34ad658b1f416d5db90
stubit <haas.fabian@gmx.de>
2020-12-05 12:56:50 +0100

    fix keeping course selection state after logout

--- src/components/Setup.vue
+++ src/components/Setup.vue
@@ -298,6 +298,7 @@ export default {
           this.$unbind('config', () => {
           })
           this.$unbind('rawCourses', () => [])
+          this.coursesState = {}
 
           this.configState = {}
         }
