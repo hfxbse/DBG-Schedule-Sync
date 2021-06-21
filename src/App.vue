716248aec1b366b7e50022470e9cc48408e68f74
Fabian Haas <29468630+StuBITtv@users.noreply.github.com>
2021-06-22 00:23:44 +0200

    add link header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
   <div id="app">
+    <div class="links">
+      <a href="/privacy.pdf">Datenschutz</a>
+      <a class="github" href="https://github.com/StuBITtv/DBG-Schedule-Sync">
+        <img alt="GitHub Icon" src="@/assets/GitHub.svg">
+        <span>Quellcode</span>
+      </a>
+    </div>
     <home/>
   </div>
 </template>
@@ -20,6 +27,55 @@ html, body, #app {
   --ink-blue: #3351BB
 }
 </style>
+
+<style scoped>
+.links {
+  position: fixed;
+  top: 1.7rem;
+  right: 1rem;
+
+  z-index: 10;
+
+  display: flex;
+  justify-content: right;
+}
+
+@media (max-width: 50ch) {
+  .links {
+    top: 5rem
+  }
+}
+
+.github {
+  width: fit-content;
+  display: flex;
+}
+
+img {
+  height: 1.25rem;
+  margin-right: 0.5rem;
+}
+
+span {
+  margin: auto;
+}
+
+a {
+  color: black;
+  text-decoration: none;
+
+  position: relative;
+  text-align: right;
+}
+
+a:first-child {
+  margin-right: 0.75rem;
+  padding-right: 0.75rem;
+
+  border-right: 1px solid black;
+}
+</style>
+
 <script>
 import Home from "./views/Home";
 
