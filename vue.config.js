8b5797944e33d4902f1bfe5a2e30327e9099efe2
Fabian Haas <29468630+StuBITtv@users.noreply.github.com>
2021-06-25 22:15:22 +0200

    add link preview image

--- vue.config.js
+++ vue.config.js
@@ -11,6 +11,7 @@ module.exports = {
         index: {
             entry: 'src/main.js',
             title: 'DBG Vertretungsplan Synchronisation',
+            preview: process.env.PREVIEW_IMAGE,
             googleVerificationIdOne: process.env.GOOGLE_VERIFICATION_ID_ONE,
             googleVerificationIdTwo: process.env.GOOGLE_VERIFICATION_ID_TWO
         }
